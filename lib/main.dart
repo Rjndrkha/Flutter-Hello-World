45bcf5781c7f57773912a7721d6af4878e6a4477
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-09-29 18:09:17 +0700

    FIX

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Flutter Hello World',
+              'Rajendra Rakha A P',
+            ),
+            const Text(
+              '1941720080',
             ),
             Text(
               '$_counter',
@@ -105,11 +108,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
